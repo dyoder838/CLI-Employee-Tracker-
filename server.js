20db5631c03fe7a829f7135dc6b3581ce46f021e
dyoder838 <70074229+dyoder838@users.noreply.github.com>
2020-10-13 19:16:56 -0700

    added inquirer start path for adding, viewing and updating

--- server.js
+++ server.js
@@ -18,12 +18,96 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
+  console.log("connected")
   start();
 });
 
 function start(){
-console.log("connected")
+
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Good day User! what would you like to do?",
+            name:"start",
+            choices: ["Add departments, roles, and employees.", "View departments, roles, and employees.", "update roles."]
+        },
+    ]).then(function (res) {
+            
+        switch(res.start){
+            case "Add departments, roles, and employees.":
+                addThings();
+                break;
+                
+            case "View departments, roles, and employees.":
+                viewThings();
+                break;
+    
+            case "update roles.":
+                updateThings();
+                break;
+    
+                default: 
+                console.log("Something went wrong!");
+                break;
+        };
+
+    })   
 }
+//TODO: addThings function 
+function addThings(){
+    
+    console.log("adding things");
+    
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to ADD?",
+            name: "addList",
+            choices: ["Add departments", "Add roles", "Add employees"]
+        }
+    ])
+}
+
+//TODO: viewThings function
+function viewThings(){
+    
+    console.log("viewing things");
+   
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to VIEW?",
+            name: "viewList",
+            choices: ["View departments", "View roles", "View employees"]     
+        }
+    ])
+} 
+
+//TODO: updateThings function 
+function updateThings(){
+    
+    console.log("updating things");
+    
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to UPDATE?",
+            name: "updateList",
+            choices: ["Update roles"]   
+        }
+    ])
+}
+
+
+
+
+
+     //.then(function(answers){
+    //     console.log(answers)
+        
+    //     console.log(generateHtml)
+    // })
+
 
 // to get an array of all departments
 
